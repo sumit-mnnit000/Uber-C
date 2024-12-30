11fac1942e99e63418466f1c082f35214041a76d
Sumit Kasaudhan <sumitkasaudhan526@gmail.com>
2024-12-30 21:10:16 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,9 @@
+const dotenv= require('dotenv');
+dotenv.config();
+const express= require('express');
+const app= express();
+
+app.get('/', (req,res)=>{
+    res.send("Hello World");
+});
+module.exports= app;
