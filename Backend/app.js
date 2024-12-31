c4f3a218077cb73ae7051b4931ab98a370e212c0
Sumit Kasaudhan <sumitkasaudhan526@gmail.com>
2024-12-31 13:41:56 +0530

    user registration done

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,23 @@
 const dotenv= require('dotenv');
 dotenv.config();
+const cors= require('cors');
+
 const express= require('express');
 const app= express();
+const connectToDb= require('./Db/db');
+const userRoutes= require('./routes/user.routes');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+connectToDb();
+app.use(cors());
+
+
+app.use('/users', userRoutes);
 app.get('/', (req,res)=>{
     res.send("Hello World");
 });
+
+
 module.exports= app;
